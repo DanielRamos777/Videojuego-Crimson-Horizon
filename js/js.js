804b846c51a64c71000fc671ed9e9ae93e467520
Daniel Ramos <oscardanielramosvillalobos@gmail.com>
2024-10-10 15:08:56 -0500

    10/10/2024

---
+++ js/js.js
@@ -0,0 +1,20 @@
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-links');
+
+menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+//------------------------
+// Esperar 5 segundos antes de mostrar el contenido
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        // Ocultar el preloader
+        document.getElementById("preloader").style.display = "none";
+
+        // Mostrar el contenido y aplicar la transición de opacidad
+        const contenido = document.getElementById("contenido");
+        contenido.style.display = "block";
+        contenido.style.opacity = "1";
+    }, 1500); 
+});
