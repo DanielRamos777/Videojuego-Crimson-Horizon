07e7e322e05fab696b765c0894283ef05859fdd3
Daniel Ramos <oscardanielramosvillalobos@gmail.com>
2024-10-14 14:50:47 -0500

    14/10/2024

---
+++ js/mapas.js
@@ -0,0 +1,5 @@
+document.getElementById('mapa-ciudad').addEventListener('click', function() {
+    const infoCiudad = document.getElementById('info-ciudad');
+    infoCiudad.classList.toggle('hidden');
+  });
+  
