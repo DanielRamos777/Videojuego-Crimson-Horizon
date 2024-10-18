c82f32bd0441dcdcd8d98e12155f94d888fb5c58
Daniel Ramos <oscardanielramosvillalobos@gmail.com>
2024-10-17 22:20:42 -0500

    17/10/2024

--- js/inicio.js
+++ js/inicio.js
@@ -1,29 +1,20 @@
-let currentIndex = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  const counters = document.querySelectorAll('.counter');
+  counters.forEach(counter => {
+      const updateCount = () => {
+          const target = +counter.getAttribute('data-count');
+          const count = +counter.innerText;
 
-function showSlide(index) {
-  const slides = document.querySelectorAll('.carousel-item');
-  const totalSlides = slides.length;
-  
-  // Asegura que el índice esté dentro de los límites
-  if (index >= totalSlides) {
-    currentIndex = 0;
-  } else if (index < 0) {
-    currentIndex = totalSlides - 1;
-  } else {
-    currentIndex = index;
-  }
+          const speed = 200;
+          const increment = target / speed;
 
-  const offset = -currentIndex * 100;
-  document.querySelector('.carousel-inner').style.transform = `translateX(${offset}vw)`;
-}
-
-function nextSlide() {
-  showSlide(currentIndex + 1);
-}
-
-function prevSlide() {
-  showSlide(currentIndex - 1);
-}
-
-// Inicializa la visualización de la primera diapositiva
-showSlide(currentIndex);
+          if (count < target) {
+              counter.innerText = Math.ceil(count + increment);
+              setTimeout(updateCount, 20);
+          } else {
+              counter.innerText = target;
+          }
+      };
+      updateCount();
+  });
+});
